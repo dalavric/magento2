c5061f7bbeb664d97b1c4d8b1b34bf4644791044
Daniel Lavric <dalavric@yahoo.com>
2017-10-19 12:54:24 +0300

    Initial Commit

---
+++ code/DD/Reservation/registration.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Daniel
+ * Date: 18.10.2017
+ * Time: 12:16
+ */
+
+\Magento\Framework\Component\ComponentRegistrar::register(
+        \Magento\Framework\Component\ComponentRegistrar::MODULE,
+        'DD_Reservation',
+        __DIR__
+    );
---
+++ etc/env.php
@@ -0,0 +1,63 @@
+<?php
+return array (
+  'backend' => 
+  array (
+    'frontName' => 'admin',
+  ),
+  'crypt' => 
+  array (
+    'key' => '520e225d82da802548e0b8e39e695117',
+  ),
+  'session' => 
+  array (
+    'save' => 'files',
+  ),
+  'db' => 
+  array (
+    'table_prefix' => '',
+    'connection' => 
+    array (
+      'default' => 
+      array (
+        'host' => '127.0.0.1:3306',
+        'dbname' => 'bitnami_magento',
+        'username' => 'bn_magento',
+        'password' => 'cbb7e7c32b',
+        'model' => 'mysql4',
+        'engine' => 'innodb',
+        'initStatements' => 'SET NAMES utf8;',
+        'active' => '1',
+      ),
+    ),
+  ),
+  'resource' => 
+  array (
+    'default_setup' => 
+    array (
+      'connection' => 'default',
+    ),
+  ),
+  'x-frame-options' => 'SAMEORIGIN',
+  'MAGE_MODE' => 'developer',
+  'cache_types' => 
+  array (
+    'config' => 1,
+    'layout' => 1,
+    'block_html' => 1,
+    'collections' => 1,
+    'reflection' => 1,
+    'db_ddl' => 1,
+    'eav' => 1,
+    'customer_notification' => 1,
+    'full_page' => 1,
+    'config_integration' => 1,
+    'config_integration_api' => 1,
+    'translate' => 1,
+    'config_webservice' => 1,
+    'compiled_config' => 1,
+  ),
+  'install' => 
+  array (
+    'date' => 'Tue, 17 Oct 2017 13:57:08 +0000',
+  ),
+);
